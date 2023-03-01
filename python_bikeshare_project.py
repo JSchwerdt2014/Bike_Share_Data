1d3b9efccfa25c064a5ab62d11759b99677a8b1a
Justin Schwerdt <schwerdty2014@gmail.com>
2023-02-28 20:33:18 -0600

    Remove unnecessary comments from python_bikeshare_project

--- python_bikeshare_project.py
+++ python_bikeshare_project.py
@@ -65,13 +65,6 @@ def time_stats(df):
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
 
-    # TO DO: display the most common month
-
-    # TO DO: display the most common day of week
-
-
-    # TO DO: display the most common start hour
-
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
@@ -82,14 +75,6 @@ def station_stats(df):
 
     print('\nCalculating The Most Popular Stations and Trip...\n')
     start_time = time.time()
-
-    # TO DO: display most commonly used start station
-
-
-    # TO DO: display most commonly used end station
-
-
-    # TO DO: display most frequent combination of start station and end station trip
 
 
     print("\nThis took %s seconds." % (time.time() - start_time))
